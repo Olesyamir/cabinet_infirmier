29eba9d85b0d2b2e07630323354ec232a8d4ee1e
Olesyamir <olesya.miroshko@gmail.com>
2024-11-23 22:36:12 +0100

    program - isNumeroValide

--- Program.cs
+++ Program.cs
@@ -22,14 +22,76 @@ bool HasAdresse(string xmlFilePath, string myXpathExpression, string nsURI)
     return nlAdresseDOM[0].ChildNodes.Count > 3;
 }// fin de foction HasAdresse
 
-Boolean isNumeroValide(string nomPatient)
+bool isNumeroValide(string nomPatient)
 {
-    DOM2Xpath patientDOM = new DOM2Xpath("./data/xml/cabinet.xml");
-    string myXpathExpression = "//med:cabinet/med:patients/med:patient[med:nom='" + nomPatient + "']/med:numero";
-    XmlNodeList nlNumeroDOM = patientDOM.GetXPath ("med", "http://www.univ-grenoble-alpes.fr/l3miage/medical", myXpathExpression);
+    // DOM2Xpath patientDOM = new DOM2Xpath("./data/xml/cabinet.xml");
+    // string myXpathExpression = "//med:cabinet/med:patients/med:patient[med:nom='" + nomPatient + "']";
+    // XmlNodeList nlNumeroDOM = patientDOM.GetXPath ("med", "http://www.univ-grenoble-alpes.fr/l3miage/medical", myXpathExpression);
+    //
+    // string dateNaissance = nlNumeroDOM.
+    // string numero = nlNumeroDOM[0].InnerText;
+    // // logique
+    // if (numero[0] == '0')
+    // return false;
     
-    string numero = nlNumeroDOM[0].InnerText;
-    // logique
+    // Initialiser l'accès au fichier XML via DOM2Xpath
+    // Charger le document XML
+    XmlDocument xmlDoc = new XmlDocument();
+    xmlDoc.Load("./data/xml/cabinet.xml");
+
+    // Configurer le gestionnaire des espaces de noms
+    XmlNamespaceManager namespaceManager = new XmlNamespaceManager(xmlDoc.NameTable);
+    namespaceManager.AddNamespace("med", "http://www.univ-grenoble-alpes.fr/l3miage/medical");
+
+    // Construire l'expression XPath pour trouver le patient
+    string myXpathExpression = "//med:cabinet/med:patients/med:patient[med:nom='" + nomPatient + "']";
+
+    // Récupérer le nœud correspondant au patient
+    XmlNode patientNode = xmlDoc.SelectSingleNode(myXpathExpression, namespaceManager);
+
+    // Vérifier si le patient existe
+    if (patientNode == null)
+    {
+        // Patient introuvable
+        return false;
+    }
+
+    // Récupérer les informations du patient
+    XmlNode sexeNode = patientNode.SelectSingleNode("med:sexe", namespaceManager);
+    XmlNode numeroNode = patientNode.SelectSingleNode("med:numero", namespaceManager);
+    XmlNode naissanceNode = patientNode.SelectSingleNode("med:naissance", namespaceManager);
+
+    if (sexeNode == null || numeroNode == null)
+    {
+        // Si l'une des informations est manquante
+        return false;
+    }
+
+    string sexe = sexeNode.InnerText;
+    string numero = numeroNode.InnerText;
+    string naissance = naissanceNode.InnerText;
+
+    // Extraire les informations de naissance
+    string[] dateNaissance = naissance.Split('-');
+    string anneeNaissance = dateNaissance[0];
+    string moisNaissance = dateNaissance[1].PadLeft(2, '0');  // Ajouter un zéro devant si nécessaire
+    string jourNaissance = dateNaissance[2].PadLeft(2, '0');  // Ajouter un zéro devant si nécessaire
+
+    // Extraire les deux derniers chiffres de l'année de naissance
+    string derniersChiffresAnnee = anneeNaissance.Substring(2, 2);
+
+    // Vérifier les conditions
+    bool conditionSexe = (sexe == "M" && numero[0] == '1') || (sexe == "F" && numero[0] == '2');
+    bool conditionDate = numero.Substring(1, 2) == derniersChiffresAnnee;
+    bool conditionMois = numero.Substring(3, 2) == moisNaissance;
+    bool conditionJour = numero.Substring(5, 2) == jourNaissance;
+
+    // Si toutes les conditions sont remplies, retourner true
+    if (conditionSexe && conditionDate && conditionMois && conditionJour)
+    {
+        return true;
+    }
+
     return false;
 
 }// fin de foction isNumeroValide
@@ -68,4 +130,4 @@ AppContext.SetSwitch("Switch.System.Xml.AllowDefaultResolver", true);
 // Console.WriteLine("Adresse est complet : {0}",HasAdresse(xmlPathCabinet, "//med:cabinet/med:patients/med:patient[med:nom='alecole']/med:adresse", URICabinet));
 // 
 
-// isNumeroValide("alecole");
+Console.WriteLine(isNumeroValide("Orouge"));
